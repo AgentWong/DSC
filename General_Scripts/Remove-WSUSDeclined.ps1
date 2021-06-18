0cd2efa4d98f0f55f9c84d2726a44989e349bcaf
Herman Wong <herman.wong1.ctr@navy.mil>
2021-06-17 15:24:27 -1000

    WSUS Declined.

---
+++ General_Scripts/Remove-WSUSDeclined.ps1
@@ -0,0 +1,13 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+Get-WsusUpdate -Classification All -Approval Unapproved -Status Any | Deny-WsusUpdate
+
+[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
+$declined=$wsus.GetUpdates().Where{$_.IsDeclined -eq $true}
+foreach($obj in $Declined){$wsus.DeleteUpdate($obj.Id.UpdateId.ToString()); Write-Host $obj.Title removed }
